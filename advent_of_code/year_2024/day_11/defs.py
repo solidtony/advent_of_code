983b54416d380c683984e0a312c0cbdf3fb3e342
solidtony <anthonygarcia918@hotmail.com>
2024-12-14 11:46:06 -0700

    Completed day_11

--- advent_of_code/year_2024/day_11/defs.py
+++ advent_of_code/year_2024/day_11/defs.py
@@ -1,27 +1,48 @@
 from pathlib import Path
 from typing import Generator
-from typing import List
+from typing import Dict
 
 
-def parse_input(input_path:Path) -> List[int]:
-    return [int(element)
-        for line in open(input_path, 'r')
-        for element in line.strip().split()
-    ]
+def parse_input(input_path:Path) -> Dict[int, int]:
+    stones = {}
+    for line in open(input_path, 'r'):
+        for stone in line.strip().split():
+            stones[int(stone)] = 1 + stones.get(stone, 0)
+    return stones
 
-def iter_steps(stones:List[int], n_steps:int) -> Generator[List[int], None, None]:
-    new_stones = stones.copy()
+def run(stones:Dict[int, int], n_steps:int) -> int:
     for step in range(n_steps):
-        for index in range(len(new_stones)-1, -1, -1):
-            stone = new_stones[index]
+        print(f'running: {step+1}/{n_steps}', end='\r')
+        items = [(stone, count) for stone, count in stones.items()]
+        for stone, count in items:
             stone_str = str(stone)
             if stone == 0:
-                new_stones[index] = 1
+                remove_stones(stones, stone, count)
+                add_stones(stones, 1, count)
             elif len(stone_str)%2 == 0:
                 half_index = int(len(stone_str)/2)
-                new_stones[index] = int(stone_str[half_index:])
-                new_stones.insert(index, int(stone_str[0:half_index]))
+                stone_1 = int(stone_str[0:half_index])
+                stone_2 = int(stone_str[half_index:])
+                remove_stones(stones, stone, count)
+                add_stones(stones, stone_1, count)
+                add_stones(stones, stone_2, count)
             else:
-                new_stones[index] = stone * 2024
-        yield new_stones
-    return None
+                new_stone = stone * 2024
+                remove_stones(stones, stone, count)
+                add_stones(stones, new_stone, count)
+    print('', end='\n')
+    return count_stones(stones)
+
+def count_stones(stones:Dict[int, int]) -> int:
+    total = 0
+    for count in stones.values():
+        total += count
+    return total
+
+def remove_stones(stones:Dict[int, int], stone:int, count:int):
+    stones[stone] = stones.get(stone, 1) - count
+    if stones[stone] < 1:
+        del stones[stone]
+
+def add_stones(stones:Dict[int, int], stone:int, count:int):
+    stones[stone] = count + stones.get(stone, 0)
--- advent_of_code/year_2024/day_11/part_1.py
+++ advent_of_code/year_2024/day_11/part_1.py
@@ -1,14 +1,11 @@
 from advent_of_code.utils import get_default_input_path
 
 from .defs import parse_input
-from .defs import iter_steps
+from .defs import run
 
 def part_1(input_path:str=get_default_input_path(__file__)):
     print(part_1_solve(input_path))
 
-n_stones = 0
 def part_1_solve(input_path:str) -> int:
     input_data = parse_input(input_path)
-    for stones in iter_steps(input_data, 25):
-        n_stones = len(stones)
-    return n_stones
+    return run(input_data, 25)
